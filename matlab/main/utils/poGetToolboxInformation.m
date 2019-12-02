1c2afcf3bb12937c95bec5430a1170a87af6ab66
os-popt <11749325@mail.sustech.edu.cn>
2019-12-02 13:44:47 +0800

    add matlab/main/utils/poGetToolboxInformation.m

---
+++ matlab/main/utils/poGetToolboxInformation.m
@@ -0,0 +1,8 @@
+function info = poGetToolboxInformation()
+info.name = 'pop-lso';
+info.homepage = 'https://github.com/os-popt/pop-lso';
+info.copyright = 'https://github.com/os-popt/pop-lso/blob/master/LICENSE';
+info.developer = 'the Open-Source POPulation-based OPTimization (os-popt) project team';
+info.version = '1.00.000.1';
+info.createDate = 'Dec 2, 2019 (20191002)';
+end
