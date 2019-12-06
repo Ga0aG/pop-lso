1eaaf432ccc08a4a9a7937938cefba04406a00a2
os-popt <11749325@mail.sustech.edu.cn>
2019-12-06 22:42:07 +0800

    add matlab/main/utils/check/popIsMatrixEquality.m

---
+++ matlab/main/utils/check/popIsMatrixEquality.m
@@ -0,0 +1,25 @@
+function TF = popIsMatrixEquality(A, B, tol)
+% Determine matrix equality within the toleration.
+%
+% Input ->
+%   A   : Input to be compared, specified as a matrix.
+%   B   : Input to be compared, specified as a matrix.
+%   tol : Comparison tolerance, specified as a non-negative scalar.
+%         If no value is specified, then the default is 1e-12.
+% Output <-
+%   TF : Returns logical 1 (true) if A and B are equal within the toleration.
+%        Otherwise, it returns logical 0 (false).
+if any(size(A) ~= size(B))
+    error('`size(A)` should equal `size(B)`.');
+end
+
+if nargin < 3
+    tol = 1e-12;
+end
+if ~poIsNonNegativeScalar(tol)
+    error('the input `tol` should be a non-negative scalar.');
+end
+
+TF = abs(A - B) <= tol;
+TF = all(TF(:));
+end
