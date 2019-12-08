61692d2f5b2c1acaa984be8643e92465f4e7b0ab
os-popt <11749325@mail.sustech.edu.cn>
2019-12-08 21:30:50 +0800

    add matlab/main/utils/check/poIsProblemName.m

---
+++ matlab/main/utils/check/poIsProblemName.m
@@ -0,0 +1,18 @@
+function TF = poIsProblemName(A)
+% Determine whether input is the problem name.
+%
+% Input ->
+%   A : Input array, specified as a scalar, vector, matrix, or multidimensional array.
+% Output <-
+%   TF : Returns logical 1 (ture) if input is the problem name.
+%        Otherwise, it returns logical 0 (false).
+if ischar(A)
+    TF = true;
+else
+    if isscalar(A) && isstring(A)
+        TF = true;
+    else
+        TF = false;
+    end
+end
+end
---
+++ matlab/test/utils/check/poIsProblemNameTest.m
@@ -0,0 +1,12 @@
+clear; clc;
+
+%% true (1)
+disp(poIsProblemName('abc'));
+disp(poIsProblemName("abc"));
+
+%% false (0)
+disp(poIsProblemName(0));
+disp(poIsProblemName([]));
+disp(poIsProblemName({}));
+disp(poIsProblemName(["abc" "xyz"]));
+disp(poIsProblemName({"abc"; "xyz"}));
