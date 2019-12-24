c4e0f5c38a7647919b8396ee8277df7d915abf0a
os-popt <11749325@mail.sustech.edu.cn>
2019-12-24 23:17:27 +0800

    add matlab/main/benchmarks/shifted/rosenbrock/

---
+++ matlab/main/benchmarks/shifted/rosenbrock/poShiftedRosenbrock.m
@@ -0,0 +1,21 @@
+function y = poShiftedRosenbrock(X)
+% Benchmark Function: Shifted Rosenbrock.
+%
+% Input ->
+%   X : Population, specified as a matrix whose row and column size
+%       equal function dimension (F) and population size (P), respectively.
+%       Each column of X denotes an independent individual in the population.
+% Output <-
+%   y : Fitness, specified as a row vector whose length is population size.
+%       Each element of y is the fitness of each individual (column) of X.
+persistent poShiftVector;
+[funcDim, popSize] = size(X);
+if funcDim < 2
+    error('`size(X, 1)` should be greater than 1');
+end
+if isempty(poShiftVector) || any(size(poShiftVector) ~= [funcDim popSize])
+    poShiftVector = poLoadShiftVector('poRosenbrock', funcDim);
+    poShiftVector = repmat(poShiftVector, 1, popSize);
+end
+y = poRosenbrock(X - poShiftVector);
+end
