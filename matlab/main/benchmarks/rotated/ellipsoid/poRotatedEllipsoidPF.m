6038f28950ebfe64bc9bf28d4f99d813d4ebe89a
os-popt <11749325@mail.sustech.edu.cn>
2019-12-27 22:44:32 +0800

    add matlab/main/benchmarks/rotated/ellipsoid/

---
+++ matlab/main/benchmarks/rotated/ellipsoid/poRotatedEllipsoidPF.m
@@ -0,0 +1,20 @@
+function y = poRotatedEllipsoidPF(X)
+% Benchmark Function: Rotated Ellipsoid.
+%
+% Input ->
+%   X : Population, specified as a matrix whose row and column size
+%       equal population size (P) and function dimension (F), respectively.
+%       Each row of X denotes an independent individual in the population.
+% Output <-
+%   y : Fitness, specified as a column vector whose length is population size.
+%       Each element of y is the fitness of each individual (row) of X.
+funcDim = size(X, 2);
+if funcDim < 2
+    error('`size(X, 2)` should be greater than 1.');
+end
+persistent poRotationMatrix;
+if isempty(poRotationMatrix) || any(size(poRotationMatrix) ~= [funcDim funcDim])
+    poRotationMatrix = poLoadRotationMatrix('poEllipsoid', funcDim);
+end
+y = poEllipsoidPF(X * poRotationMatrix');
+end
