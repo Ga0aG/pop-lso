0e45fb8e30d93163c09667cd241728644fb29807
os-popt <11749325@mail.sustech.edu.cn>
2020-01-15 21:54:32 +0800

    add matlab/test/benchmarks/rotated/cigar/checkBenchmark.m

---
+++ matlab/test/benchmarks/rotated/cigar/checkBenchmark.m
@@ -0,0 +1,35 @@
+clear; clc;
+
+% first, run the script called generateRotationMatrix.m, which is
+% located in the same directory.
+
+funcName = 'poRotatedCigar';
+
+%%
+tic;
+funcDims = [2 5 7 10 20 50 70 100 1e3 1e4];
+for i = 1 : numel(funcDims)
+    funcDim = funcDims(i);
+    X = zeros(funcDim, randi(1e3));
+    y = zeros(1, size(X, 2));
+    poManuallyCheckBenchmark(funcName, X, y);
+end
+toc;
+
+%%
+tic;
+poAutomaticallyCheckBenchmark(funcName);
+toc;
+
+%%
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedCigar : pass the automatic checking within the tolerance `1.00e-03`.
